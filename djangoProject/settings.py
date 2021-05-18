401012c0fd0004ef5a9cdb6c0462019dd2afd4ab
Evgenii Prosvirnin <proevgenii19@gmail.com>
2021-05-18 19:57:07 +0300

    add environ

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -20,11 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-o!a74fylmifo_t1i99lo1v_gnv#5+lnihcn5#2^$ptym1dht^_"
-SECRET_KEY = "changeme"
+#SECRET_KEY = "django-insecure-o!a74fylmifo_t1i99lo1v_gnv#5+lnihcn5#2^$ptym1dht^_"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ["*"]
 
